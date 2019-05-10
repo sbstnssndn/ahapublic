e1c2347bb04d77f99127e456ab2a7ffbf7fe988f
Cristóbal <cristobal.becerraf@usach.cl>
2019-05-09 20:54:36 -0400

    Se agregan archivos iniciales de backend

---
+++ backend/src/main/java/com/grupo1/ahainclusion/AhainclusionApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.ahainclusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhainclusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhainclusionApplication.class, args);
+	}
+
+}
