e570b29e234d916bdd72f58559673c53ecb70a83
Cristóbal <cristobal.becerraf@usach.cl>
2019-05-15 18:09:15 -0400

    Se agrega entidad usuario y conexión a bd

---
+++ backend/src/main/java/com/grupo1/ahainclusion/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.grupo1.ahainclusion.repository;
+
+import com.grupo1.ahainclusion.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
