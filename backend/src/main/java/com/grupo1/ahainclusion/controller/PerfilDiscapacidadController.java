9819f3998622311ae0410edecf577930c16ee557
Cristóbal <cristobal.becerraf@usach.cl>
2019-05-19 18:03:13 -0400

    Controlador perfil discapacidad

---
+++ backend/src/main/java/com/grupo1/ahainclusion/controller/PerfilDiscapacidadController.java
@@ -0,0 +1,37 @@
+package com.grupo1.ahainclusion.controller;
+
+import com.grupo1.ahainclusion.model.PerfilDiscapacidad;
+import com.grupo1.ahainclusion.repository.PerfilDiscapacidadRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/perfilDiscapacidad")
+public class PerfilDiscapacidadController {
+
+    @Autowired
+    private PerfilDiscapacidadRepository perfilDiscapacidadRepository;
+
+    // Agregar Perfil Discapacidad
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewPerfilDiscapacidad(@RequestParam String name) {
+    
+    PerfilDiscapacidad n = new PerfilDiscapacidad();
+    n.setName(name);
+
+    perfilDiscapacidadRepository.save(n);
+
+    return "Perfil Discapacidad Guardado";
+    }
+
+    // Obtener Perfiles Discapacidad
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<PerfilDiscapacidad> gettAllPerfilesDiscapacidad() {
+        return perfilDiscapacidadRepository.findAll();
+    }
+}
