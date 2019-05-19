8ed9c9d6448e96d08a7e595fb69a39a1645bcbff
Cristóbal <cristobal.becerraf@usach.cl>
2019-05-18 22:45:54 -0400

    Se agregan repositorios de oferta y perfiles discapacidad/accesibilidad

--- backend/src/main/java/com/grupo1/ahainclusion/model/PerfilDiscapacidad.java
+++ backend/src/main/java/com/grupo1/ahainclusion/model/PerfilDiscapacidad.java
@@ -1,5 +1,25 @@
 package com.grupo1.ahainclusion.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class PerfilDiscapacidad {
-    
+
+    @Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
