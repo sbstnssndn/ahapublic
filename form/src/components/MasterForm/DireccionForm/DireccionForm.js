04e1a2125c55e548f67da5fbc616ae05cfa24a8c
sbstnssndn <sebastian.ossandon@usach.cl>
2019-05-20 16:28:43 -0400

    Fixes menores

--- form/src/components/MasterForm/DireccionForm/DireccionForm.js
+++ form/src/components/MasterForm/DireccionForm/DireccionForm.js
@@ -6,31 +6,20 @@ const DireccionForm = (props) => {
     } 
     return(
       <React.Fragment>
-        <h3>Dirección</h3>
+        <h3>Ubicación</h3>
         <div className="form-group">
-          <label htmlFor="calle">CALLE</label>
+          <label htmlFor="direccion">DIRECCIÓN</label>
           <input
             className="form-control"
-            id="calle"
-            name="calle"
+            id="direccion"
+            name="direccion"
             type="text"
-            placeholder="Ejemplo: Pajaritos"
-            value={props.calle}
+            placeholder="Av. La Florida 1234"
+            value={props.direccion}
             onChange={props.handleChange}
             />
         </div>
-        <div className="form-group">
-          <label htmlFor="numeroCalle">NÚMERO</label>
-          <input
-            className="form-control"
-            id="numeroCalle"
-            name="numeroCalle"
-            type="text"
-            placeholder="Ejemplo: 755"
-            value={props.numeroCalle}
-            onChange={props.handleChange}
-            />
-        </div>
+        
         <div className="form-group">
           <label htmlFor="numeroCasa">CASA/DPTO./BLOCK</label>
           <input
@@ -59,33 +48,36 @@ const DireccionForm = (props) => {
             <option value="metropolitana">Región Metropolitana</option>
           </select> 
         </div>
-        <div className="form-group">
-          <label htmlFor="credencial">CIUDAD</label>
-          <select
-            className="form-control"
-            id="ciudad"
-            name="ciudad"
-            value={props.ciudad}
-            onChange={props.handleChangeDropdown}
-            >
-            <option value="" disabled selected>Seleccione</option>
-            <option value="santiago">Santiago</option>
-            <option value="valparaiso">Valparaíso</option>
-          </select> 
-        </div>
-        <div className="form-group">
-          <label htmlFor="credencial">COMUNA</label>
-          <select
-            className="form-control"
-            id="credencial"
-            name="comuna"
-            value={props.comuna}
-            onChange={props.handleChangeDropdown}
-            >
-            <option value="" disabled selected>Seleccione</option>
-            <option value="santiago">Santiago</option>
-            <option value="estacion central">Estación Central</option>
-          </select> 
+
+        <div class="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="credencial">CIUDAD</label>
+            <select
+              className="form-control"
+              id="ciudad"
+              name="ciudad"
+              value={props.ciudad}
+              onChange={props.handleChangeDropdown}
+              >
+              <option value="" disabled selected>Seleccione</option>
+              <option value="santiago">Santiago</option>
+              <option value="valparaiso">Valparaíso</option>
+            </select> 
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="credencial">COMUNA</label>
+            <select
+              className="form-control"
+              id="credencial"
+              name="comuna"
+              value={props.comuna}
+              onChange={props.handleChangeDropdown}
+              >
+              <option value="" disabled selected>Seleccione</option>
+              <option value="santiago">Santiago</option>
+              <option value="estacion central">Estación Central</option>
+            </select> 
+          </div>
         </div>
       </React.Fragment>
     );
--- form/src/components/MasterForm/PerfilDiscapacidadForm/PerfilDiscapacidadForm.js
+++ form/src/components/MasterForm/PerfilDiscapacidadForm/PerfilDiscapacidadForm.js
@@ -6,8 +6,9 @@ const PerfilDiscapacidadForm = (props) => {
     } 
     return(
       <React.Fragment>
+        <h3>Perfil laboral</h3>
         <div className="form-group">
-          <label htmlFor="credencial">¿Tiene credencial de discapacidad?</label>
+          <label htmlFor="credencial">¿TIENE CREDENCIAL DE DISCAPACIDAD?</label>
           <select
             className="form-control"
             id="credencial"
@@ -20,16 +21,84 @@ const PerfilDiscapacidadForm = (props) => {
           </select> 
         </div>
         <div className="form-group">
-          <label htmlFor="credencial">Pregunta</label>
-          <input
+          <label htmlFor="nivelEducacional">NIVEL EDUCACIONAL</label>
+          <select
             className="form-control"
-            id="pregunta"
-            name="pregunta"
-            type="pregunta"
-            placeholder="Pregunta genérica"
-            value={props.password}
+            id="nivelEducacional"
+            name="nivelEducacional"
+            value={props.nivelEducacional}
             onChange={props.handleChange}
-            />      
+            >
+            <option value="" disabled selected>Seleccione</option>
+            <option value="true">EDUCACIÓN BÁSICA INCOMPLETA</option>
+            <option value="false">EDUCACIÓN BÁSICA COMPLETA</option>
+            <option value="true">EDUCACIÓN MEDIA INCOMPLETA</option>
+            <option value="false">EDUCACIÓN MEDIA COMPLETA</option>
+            <option value="false">TÉCNICA INCOMPLETA</option>
+            <option value="false">TÉCNICA COMPLETA</option>
+            <option value="false">PROFESIONAL INCOMPLETA</option>
+            <option value="false">PROFESIONAL COMPLETA</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="profesionOficio">PROFESIÓN/OFICIO</label>
+          <select
+            className="form-control"
+            id="profesionOficio"
+            name="profesionOficio"
+            value={props.profesionOficio}
+            onChange={props.handleChange}
+            >
+            <option value="" disabled selected>Seleccione</option>
+            <option value="ingeco">Ingeniería Comercial</option>
+            <option value="licing">Licenciatura en Inglés</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="areaExperiencia">EXPERIENCIA LABORAL</label>
+          <select
+            className="form-control"
+            id="areaExperiencia"
+            name="areaExperiencia"
+            value={props.areaExperiencia}
+            onChange={props.handleChange}
+            >
+            <option value="" disabled selected>Área</option>
+            <option value="ingeco">FINANZAS</option>
+            <option value="licing">INFORMÁTICA</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="anosExperiencia">AÑOS DE EXPERIENCIA</label>
+          <select
+            className="form-control"
+            id="anosExperiencia"
+            name="anosExperiencia"
+            value={props.anosExperiencia}
+            onChange={props.handleChange}
+            >
+            <option value="" disabled selected>Seleccione</option>
+            <option value="0">SIN EXPERIENCIA</option>
+            <option value="1">MENOS DE 1 AÑO</option>
+            <option value="2">ENTRE 1 Y 2 AÑOS</option>
+            <option value="3">MÁS DE 2 AÑOS</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="anosExperiencia">CURSOS</label>
+          <select
+            className="form-control"
+            id="anosExperiencia"
+            name="anosExperiencia"
+            value={props.anosExperiencia}
+            onChange={props.handleChange}
+            >
+            <option value="" disabled selected>Seleccione</option>
+            <option value="0">SIN EXPERIENCIA</option>
+            <option value="1">MENOS DE 1 AÑO</option>
+            <option value="2">ENTRE 1 Y 2 AÑOS</option>
+            <option value="3">MÁS DE 2 AÑOS</option>
+          </select>
         </div>
         <button className="btn btn-success btn-block">Guardar datos</button>
         <br />
