5cf17f2288e4f30481f9ad7f89d6726ca7777b3d
Cristóbal <cristobal.becerraf@usach.cl>
2019-05-21 13:36:14 -0400

    Se agregan clases inciales para recomendaciones

---
+++ backend/src/main/java/com/grupo1/ahainclusion/recommendation/Recommendation.java
@@ -0,0 +1,9 @@
+package com.grupo1.ahainclusion.recommendation;
+
+import com.grupo1.ahainclusion.model.User;
+
+public class Recommendation {
+    private User user;
+    private Integer percentage;
+
+}
---
+++ backend/src/main/java/com/grupo1/ahainclusion/recommendation/RecommendationsGenerator.java
@@ -0,0 +1,17 @@
+package com.grupo1.ahainclusion.recommendation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.grupo1.ahainclusion.model.Oferta;
+
+public class RecommendationsGenerator {
+
+    private List<Recommendation> genRecommendations(Oferta oferta) {
+        List<Recommendation> recommendations = new ArrayList<>();
+
+        return recommendations;
+    }
+
+
+}
