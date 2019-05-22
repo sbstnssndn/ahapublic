6a6c2127b694d68d735525e5e674bfd9c306a6a5
Cristóbal <cristobal.becerraf@usach.cl>
2019-05-22 12:29:29 -0400

    API de recomendaciones funcional

--- backend/src/main/java/com/grupo1/ahainclusion/controller/RecommendationController.java
+++ backend/src/main/java/com/grupo1/ahainclusion/controller/RecommendationController.java
@@ -1,11 +1,41 @@
 package com.grupo1.ahainclusion.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.grupo1.ahainclusion.model.Oferta;
+import com.grupo1.ahainclusion.recommendation.RecGenerator;
+import com.grupo1.ahainclusion.recommendation.Recommendation;
+import com.grupo1.ahainclusion.repository.OfertaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(path="/recommendation")
 public class RecommendationController {
+
+    @Autowired
+    private OfertaRepository ofertaRepository;
+
+    @Autowired
+    private RecGenerator recgen = new RecGenerator();
+
+    // Obtener Recomendaciones
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Recommendation> getRecommendations(@RequestParam Integer idOferta,
+                                                                     @RequestParam Integer n) {
+        List<Recommendation> recommendations = new ArrayList<>();
+        Oferta oferta = ofertaRepository.findById(idOferta).get();
+
+        recommendations = recgen.generate(oferta, n);
+
+        return recommendations;
+    }
     
 
 }
--- backend/src/main/java/com/grupo1/ahainclusion/recommendation/RecGenerator.java
+++ backend/src/main/java/com/grupo1/ahainclusion/recommendation/RecGenerator.java
@@ -3,25 +3,39 @@ package com.grupo1.ahainclusion.recommendation;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import com.grupo1.ahainclusion.model.Oferta;
 import com.grupo1.ahainclusion.model.User;
 import com.grupo1.ahainclusion.repository.UserRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RecGenerator {
     
+    @Autowired
     private UserRepository userRepository;
 
-    private List<Recommendation> generate(Oferta oferta, int n) {
+    public List<Recommendation> generate(Oferta oferta, int n) {
         
+        System.out.println("----------------------------");
         Iterable<User> users = userRepository.findAll();
         List<Recommendation> recommendations = new ArrayList<>();
 
-        System.out.println("HOLAAAAAAAAAAA");
+        
         for(User u: users)
         {
-            System.out.println(u.getFirstName());
+            Recommendation rcm = new Recommendation();
+            rcm.setUser(u);
+            rcm.setPercentage(90);
+
+            recommendations.add(rcm);
+
+            System.out.println("Usuario: " +rcm.getUser().getFirstName() +" "+"Porcentaje: "+rcm.getPercentage());
         }
-        System.out.println("HOLAAAAAAAAAAA");
+        System.out.println("----------------------------");
         
         return recommendations;
     }
