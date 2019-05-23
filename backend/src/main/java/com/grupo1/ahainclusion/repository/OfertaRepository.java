aa64a195b80b35b11eb71ac704cf92a3654aca37
sbstnssndn <sebastian.ossandon@usach.cl>
2019-05-22 19:35:06 -0700

    back borrado

--- backend/src/main/java/com/grupo1/ahainclusion/repository/OfertaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.grupo1.ahainclusion.repository;
-
-import com.grupo1.ahainclusion.model.Oferta;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OfertaRepository extends CrudRepository<Oferta, Integer> {
-
-}
