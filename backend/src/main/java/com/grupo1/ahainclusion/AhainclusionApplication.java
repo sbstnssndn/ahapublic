2fdebf19878808dc569b304b9b57b87b65c4655c
Cristóbal <cristobal.becerraf@usach.cl>
2019-05-22 20:11:45 -0400

    fix cors

--- backend/src/main/java/com/grupo1/ahainclusion/AhainclusionApplication.java
+++ backend/src/main/java/com/grupo1/ahainclusion/AhainclusionApplication.java
@@ -2,12 +2,28 @@ package com.grupo1.ahainclusion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class AhainclusionApplication {
+
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(AhainclusionApplication.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
+			}
+        };
+
+	}
 }
