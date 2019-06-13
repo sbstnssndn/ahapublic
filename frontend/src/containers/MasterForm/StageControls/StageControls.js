3358d3ac6148e7b919c685d76821918af5c70ff3
sbstnssndn <sebastian.ossandon@usach.cl>
2019-06-13 01:16:39 -0400

    fix colores

--- frontend/src/containers/MasterForm/StageControls/StageControls.js
+++ frontend/src/containers/MasterForm/StageControls/StageControls.js
@@ -18,7 +18,7 @@ const StageControls = (props) => {
 	let prevButton = null;
 	if (currentStage !== firstStage) {
 		prevButton = (
-			<Button variant="secondary" onClick={props._prev} className={styles.BotonAnterior}>
+			<Button variant="secondary" onClick={props._prev}>
 				Anterior
 			</Button>
 		);
@@ -27,7 +27,7 @@ const StageControls = (props) => {
 	let nextButton = null;
 	if (currentStage < lastStage) {
 		nextButton = (
-		<Button variant="primary" onClick={props._next} className={styles.BotonSiguiente}>
+		<Button variant="primary" onClick={props._next}>
 			Siguiente
 		</Button>        
 		);
