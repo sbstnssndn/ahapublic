dd199b80d8db8dbf84e6b2c4773efdcb2ed1fb67
Cristóbal <cristobal.becerraf@usach.cl>
2019-06-13 03:32:59 -0400

    fix controller recomendaciones y json empresa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1350,29 +1350,77 @@ function App() {
 						type: "normal",
 						elements: [
 							{
-								label: "Ubicación",
+								label: "Nombre Empresa",
 								elementType: "input",
 								elementConfig: {
 									type: "text",
-									placeholder: "Av. Tobalaba 417, piso 2",
-									name: "ubicacion",
-									id: "ubicacion"
+									placeholder: "Lider S.A",
+									name: "nombre",
+									id: "nombre"
 								},
 								value: ""
 							}
 						]
 					},
-					cargo: {
+					rut: {
 						type: "normal",
 						elements: [
 							{
-								label: "Cargo",
+								label: "Rut Empresa",
 								elementType: "input",
 								elementConfig: {
 									type: "text",
-									placeholder: "Desarrollador PHP Jr.",
-									name: "cargo",
-									id: "cargo"
+									placeholder: "7546345-2",
+									name: "rut",
+									id: "rut"
+								},
+								value: ""
+							}
+						]
+					},
+					telefono1: {
+						type: "normal",
+						elements: [
+							{
+								label: "Telefono",
+								elementType: "input",
+								elementConfig: {
+									type: "text",
+									placeholder: "223546544",
+									name: "telefono1",
+									id: "telefono1"
+								},
+								value: ""
+							}
+						]
+					},
+					telefono2: {
+						type: "normal",
+						elements: [
+							{
+								label: "Telefono Alternativo",
+								elementType: "input",
+								elementConfig: {
+									type: "text",
+									placeholder: "226537434",
+									name: "telefono2",
+									id: "telefono2"
+								},
+								value: ""
+							}
+						]
+					},
+					correo2: {
+						type: "normal",
+						elements: [
+							{
+								label: "Correo Alternativo",
+								elementType: "input",
+								elementConfig: {
+									type: "text",
+									placeholder: "contacto@lider.cl",
+									name: "correo2",
+									id: "correo2"
 								},
 								value: ""
 							}
