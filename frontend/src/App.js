d1e119ee5e179e456fa0bb2d836be5294b97961f
Maximiliano Espinosa <maximiliano.espinosa@usach.cl>
2019-06-13 00:30:29 -0400

    Arreglo formCuentaUsuario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1480,6 +1480,54 @@ function App() {
 		]
 	}
 
+	const formCuentaUsuario =
+	{
+		title: "Información cuenta de usuario",
+		belongsTo: "user",
+		endpoint: "http://localhost:8080/api/user",
+		totalStages: 1,
+		stages: [
+			{
+				id: 0,
+				name: "Mi cuenta de usuario",
+				fields: {
+					email: {
+						type: 'normal',
+						elements: [
+							{
+								label: 'Correo electrónico',
+								elementType: 'input',
+								elementConfig: {
+									type: 'email',
+									placeholder: 'juan.perez@gmail.com',
+									name: 'email',
+									id: 'email'
+								},
+								value: ''
+							}
+						]
+					},
+					password: {
+						type: 'normal',
+						elements: [
+							{
+								label: 'Contraseña',
+								elementType: 'input',
+								elementConfig: {
+									type: 'password',
+									placeholder: '********',
+									name: 'password',
+									id: 'password'
+								},
+								value: ''
+							}
+						]
+					}
+				}
+			}
+		]
+	}
+
   return (
     <Routes
 		formPostulante={formPostulante}
