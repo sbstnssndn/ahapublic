350fb1fcf658a49aaaf38a364c1e36870c0d15ac
Seba <sebastian.reinoso@usach.cl>
2019-06-23 14:27:13 -0400

    Hotfix mail

--- frontend/src/containers/MasterForm/MasterForm.js
+++ frontend/src/containers/MasterForm/MasterForm.js
@@ -121,6 +121,16 @@ class MasterForm extends Component {
 						break;
 
 					case('email'):
+						const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+						const result = regex.test(String(singleField).toLowerCase())
+
+						if (result){
+							elementsArray[elem].subtext = 'Email incorrecto'
+							this.setState({missing: true})
+							break;
+						}
+						else
+							this.setState({missing: false})
 						break;
 					default:
 						if (len>255){
