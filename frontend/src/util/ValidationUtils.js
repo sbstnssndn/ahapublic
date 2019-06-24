3e3209d3cc9cf431d8fd452310a8852b4414305d
Seba <sebastian.reinoso@usach.cl>
2019-06-23 21:35:32 -0400

    Fix regex email

--- frontend/src/util/ValidationUtils.js
+++ frontend/src/util/ValidationUtils.js
@@ -1,5 +1,5 @@
 export const emailIsValid = (email) => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
 }
 
 export const passwordLengthIsValid = (text) => {
