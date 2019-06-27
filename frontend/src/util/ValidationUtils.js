5655c4262d4cc61bfbd2969b77b61218746eccc3
sebareinoso <sebastian.reinoso@usach.cl>
2019-06-27 15:55:05 -0400

    Cambios para region

--- frontend/src/util/ValidationUtils.js
+++ frontend/src/util/ValidationUtils.js
@@ -1,5 +1,3 @@
-import DatePicker from "react-datepicker";
-
 export const emailIsValid = (email) => {
   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
 }
