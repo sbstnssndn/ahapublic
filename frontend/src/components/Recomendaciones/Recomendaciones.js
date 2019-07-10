83185b3f3d6f17a6a8fd068960f99e8b42bb7134
Maximiliano Espinosa <maximiliano.espinosa@usach.cl>
2019-07-10 15:22:49 -0400

    Visible recomendaciones para una oferta, por parte de perfil aha

--- frontend/src/components/Recomendaciones/Recomendaciones.js
+++ frontend/src/components/Recomendaciones/Recomendaciones.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
-//import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-//import styles from '../Paginacion/App.module.css';
+import styles from '../Paginacion/App.module.css';
 //import { Link } from "react-router-dom";
 
 class Recomendaciones extends Component {
 
 	state = {
-		recs: []
+        recs: [],
+        oferta: []
     }
 
     componentDidMount () {
@@ -18,22 +19,67 @@ class Recomendaciones extends Component {
         	.then(response => {
 				this.setState({
 					recs: response.data
-				});
+                });
+                axios.get("http://localhost:8080/api/oferta/"+id)
+                    .then(response => {
+                        this.setState({
+                            oferta: response.data
+                        })
+                    })
+                    .catch(function(error){
+                        console.log(error)
+                    })
             })
             .catch(function(error){
             	console.log(error);
 			})
+    }
+    
+    postulante(user){
 		
+		let id = user.id;
+
+        return (
+			//<Link to={`/aha/empresas/${id_empresa}`}>
+				<Button variant="success" type="primary" size="sm">
+					Ver perfil
+				</Button>
+			//</Link>
+        );
 	}
     
     render() {
-        if(this.state.recs != null){
+        if(this.state.recs != null && this.state.oferta != null){
             return (
                 <React.Fragment>
                     <Card className="mb-4">
                         <Card.Header className="px-2">
-                            Recomendaciones: {this.state.recs.length}
+                            Recomendaciones - {this.state.oferta.name}
                         </Card.Header>
+
+                        
+                        <table className={styles.table}>
+                            <thead>
+                                <tr>
+                                    <th>Postulante </th>
+                                    <th>Compatibilidad </th>
+                                    <th>Ver</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.recs.map(user => {
+                                    return (
+                                        <tr key={user.userSummary.id}>
+                                            <td> {user.userSummary.name} </td>
+                                            <td> {user.percentage} </td>
+                                            <td> {this.postulante(user)} </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>           
+                
+
                     </Card>  
                 </React.Fragment>
             )
