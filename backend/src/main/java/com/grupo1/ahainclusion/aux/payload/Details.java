dde8f8d5f4800ae586b005c8f93acf1367a128e7
Cristóbal <cristobal.becerraf@usach.cl>
2019-07-10 21:13:18 -0400

    recomendaciones ahora retorna objeto details con mas info

---
+++ backend/src/main/java/com/grupo1/ahainclusion/aux/payload/Details.java
@@ -0,0 +1,33 @@
+package com.grupo1.ahainclusion.aux.payload;
+
+public class Details {
+
+    private String email2;
+    private String telefono1;
+    private String telefono2;
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public String getTelefono2() {
+        return telefono2;
+    }
+
+    public void setTelefono2(String telefono2) {
+        this.telefono2 = telefono2;
+    }
+
+    public String getTelefono1() {
+        return telefono1;
+    }
+
+    public void setTelefono1(String telefono1) {
+        this.telefono1 = telefono1;
+    }
+
+    public void setEmail2(String email2) {
+        this.email2 = email2;
+    }
+
+}
